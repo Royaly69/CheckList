43994d3304ec7a9ac71b134e4f2b8038bef88c5e
iowns <iowns@ROYALY>
2024-10-02 09:05:13 +0200

    Add project files.

---
+++ CheckList/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Collections.Generic;
+string ans;
+Console.WriteLine("Indtast på en linje en motivationsfaktor");
+ans = Console.ReadLine();
+
+if(CheckList(ans))
+{
+    Console.WriteLine("Faktor er i liste");
+}
+else
+{
+    Console.WriteLine("Faktor er ikke i liste");
+}
+static bool CheckList(string faktor)
+{
+    bool s;
+    List<string> list = new List<string>() { "løn", "anerkendelse", "mål", "autonomi", "udvikling" };
+    foreach (string f in list)
+    {
+        if (faktor.ToLower() == f)
+        {
+            return s = true;
+        }
+    }
+    return s=false;
+}
+
+    
